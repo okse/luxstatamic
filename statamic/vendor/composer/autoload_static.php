c97ce3dda2f188d47182134591fe29b631e2bfeb
Erling Håmsø <erling@okse.no>
2018-10-19 13:17:15 +0200

    Update statamic 2.10.7

--- statamic/vendor/composer/autoload_static.php
+++ statamic/vendor/composer/autoload_static.php
@@ -4,26 +4,26 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit8c26a195a3fdbbeecf5f03f77d4d7be5
+class ComposerStaticInitfac2c5b84ddd2671641040439a58937d
 {
     public static $files = array (
         '95a22d2145ae40de20dc7ea7be07a610' => __DIR__ . '/../..' . '/bootstrap/helpers.php',
-        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
-        '667aeda72477189d0494fecd327c3641' => __DIR__ . '/..' . '/symfony/var-dumper/Resources/functions/dump.php',
         'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
+        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
         'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
         '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
+        '667aeda72477189d0494fecd327c3641' => __DIR__ . '/..' . '/symfony/var-dumper/Resources/functions/dump.php',
         'b067bc7112e384b61c701452d53a14a8' => __DIR__ . '/..' . '/mtdowling/jmespath.php/src/JmesPath.php',
         '8a9dc1de0ca7e01f3e08231539562f61' => __DIR__ . '/..' . '/aws/aws-sdk-php/src/functions.php',
-        '2c102faa651ef8ea5874edb585946bce' => __DIR__ . '/..' . '/swiftmailer/swiftmailer/lib/swift_required.php',
         '5255c38a0faeba867671b61dfda6d864' => __DIR__ . '/..' . '/paragonie/random_compat/lib/random.php',
+        '2c102faa651ef8ea5874edb585946bce' => __DIR__ . '/..' . '/swiftmailer/swiftmailer/lib/swift_required.php',
         'bd9634f2d41831496de0d3dfe4c94881' => __DIR__ . '/..' . '/symfony/polyfill-php56/bootstrap.php',
-        'e7223560d890eab89cda23685e711e2c' => __DIR__ . '/..' . '/psy/psysh/src/Psy/functions.php',
         '65fec9ebcfbb3cbb4fd0d519687aea01' => __DIR__ . '/..' . '/danielstjules/stringy/src/Create.php',
+        'e7223560d890eab89cda23685e711e2c' => __DIR__ . '/..' . '/psy/psysh/src/Psy/functions.php',
         '023d27dca8066ef29e6739335ea73bad' => __DIR__ . '/..' . '/symfony/polyfill-php70/bootstrap.php',
+        '4a1f389d6ce373bda9e57857d3b61c84' => __DIR__ . '/..' . '/barryvdh/laravel-debugbar/src/helpers.php',
         'f0906e6318348a765ffb6eb24e0d0938' => __DIR__ . '/..' . '/laravel/framework/src/Illuminate/Foundation/helpers.php',
         '58571171fd5812e6e447dce228f52f4d' => __DIR__ . '/..' . '/laravel/framework/src/Illuminate/Support/helpers.php',
-        '4a1f389d6ce373bda9e57857d3b61c84' => __DIR__ . '/..' . '/barryvdh/laravel-debugbar/src/helpers.php',
         'f18cc91337d49233e5754e93f3ed9ec3' => __DIR__ . '/..' . '/laravelcollective/html/src/helpers.php',
         '04c6c5c2f7095ccf6c481d3e53e1776f' => __DIR__ . '/..' . '/mustangostang/spyc/Spyc.php',
     );
@@ -279,8 +279,8 @@ class ComposerStaticInit8c26a195a3fdbbeecf5f03f77d4d7be5
         'League\\Glide\\' => 
         array (
             0 => __DIR__ . '/..' . '/league/glide/src',
-            1 => __DIR__ . '/..' . '/league/glide-symfony/src',
-            2 => __DIR__ . '/..' . '/league/glide-laravel/src',
+            1 => __DIR__ . '/..' . '/league/glide-laravel/src',
+            2 => __DIR__ . '/..' . '/league/glide-symfony/src',
         ),
         'League\\Flysystem\\ZipArchive\\' => 
         array (
@@ -3779,10 +3779,10 @@ class ComposerStaticInit8c26a195a3fdbbeecf5f03f77d4d7be5
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit8c26a195a3fdbbeecf5f03f77d4d7be5::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit8c26a195a3fdbbeecf5f03f77d4d7be5::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInit8c26a195a3fdbbeecf5f03f77d4d7be5::$prefixesPsr0;
-            $loader->classMap = ComposerStaticInit8c26a195a3fdbbeecf5f03f77d4d7be5::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitfac2c5b84ddd2671641040439a58937d::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitfac2c5b84ddd2671641040439a58937d::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInitfac2c5b84ddd2671641040439a58937d::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInitfac2c5b84ddd2671641040439a58937d::$classMap;
 
         }, null, ClassLoader::class);
     }
